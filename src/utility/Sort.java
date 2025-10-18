daaa8a0dda0bf513ab1f199ddca4e3f907fff885
Rocky McGoey <mcgoeyrocky@gmail.com>
2025-10-17 18:20:40 -0600

    Added Selection and Merge sorts

--- src/utility/Sort.java
+++ src/utility/Sort.java
@@ -75,9 +75,95 @@ public class Sort
         }
 	}
 	
+	// Selection sort
+	public static <T extends Comparable<T>> void selectionSort(T[] arr)
+	{
+        if (arr == null || arr.length < 2) 
+        	return;
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++) 
+            {
+                if (arr[j].compareTo(arr[minIndex]) < 0) 
+                {
+                    minIndex = j;
+                }
+            }
+            swap(arr, i, minIndex);
+        }
+	}
 	
+	public static <T> void SelectionSort(T[]arr, Comparator<T> c)
+	{
+        if (arr == null || arr.length < 2) 
+        	return;
+        for (int i = 0; i < arr.length - 1; i++)
+        {
+            int minIndex = i;
+            for (int j = i + 1; j < arr.length; j++)
+            {
+                if (c.compare(arr[j], arr[minIndex]) < 0)
+                {
+                    minIndex = j;
+                }
+            }
+            swap(arr, i, minIndex);
+        }
+	}
 	
-// helper method to save time for swapping
+	// Merge sort
+	public static <T extends Comparable<T>> void mergeSort(T[] arr)
+	{
+		if (arr == null || arr.length < 2)
+			return;
+		T[] left = (T[]) new Object[arr.length / 2];
+		T[] right = (T[]) new Object[arr.length - left.length];
+		
+		// Divide array into two halves
+		for (int i = 0; i < left.length; i++) left[i] = arr[i];
+		for (int i = 0; i < right.length; i++) right[i] = arr[left.length + 1];
+		
+		// Recursively sort both halves
+		mergeSort(left);
+		mergeSort(right);
+		
+		//Merge the sorted halves
+		int i = 0, j = 0, p = 0;
+		while (i < left.length && j < right.length)
+		{
+			if (left[i].compareTo(right[j]) <= 0) arr[p++] = left[i++];
+			else arr[p++] = right[j++];
+		}
+		while (i < left.length) arr[p++] = left[i++];
+		while (j < left.length) arr[p++] = right[j++];
+	}
+	
+	public static <T> void mergeSort(T[]arr, Comparator<T> c)
+	{
+		if (arr == null || arr.length < 2)
+			return;
+		T[] left = (T[]) new Object[arr.length / 2];
+		T[] right = (T[]) new Object[arr.length - left.length];
+		
+		// Divide array into two halves
+		for (int i = 0; i < left.length; i++) left[i] = arr[i];
+		for (int i = 0; i < right.length; i++) right[i] = arr[left.length + 1];
+		
+		mergeSort(left, c);
+		mergeSort(right, c);
+		
+		int i = 0, j = 0, p = 0;
+		while (i < left.length && j < right.length)
+		{
+			if (c.compare(left[i], right[j]) <= 0) arr[p++] = left[i++];
+			else arr[p++] = right[j++];
+		}
+		while (i < left.length) arr[p++] = left[i++];
+		while (j < left.length) arr[p++] = right[j++];
+	}
+	
+// Helper method to save time for swapping in sorts
 private static <T> void swap(T[] arr, int j, int i)
 {
 	T temp = arr[i];
